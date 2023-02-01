c678bfe42f99d3a26e9780b61f84dec573540f80
Youva GAUDE <ygaude@student.42.fr>
2023-02-01 15:59:58 +0100

    rename Taskmaster to taskmaster, add signal-hook as a dependency, and add some test dummy main.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+use std::io::Write;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+fn main() -> Result<(), Error> {
+    let term = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGHUP, Arc::clone(&term))?;
+    println!("hi");
+    while !term.load(Ordering::Relaxed) {
+        // Do some time-limited stuff here
+        // (if this could block forever, then there's no guarantee the signal will have any
+        // effect).
+        print!(".");
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        std::io::stdout().flush()?;
+    }
+    println!("\nbye");
+    term.store(false, Ordering::Relaxed);
+    println!("or not");
+    while !term.load(Ordering::Relaxed) {
+        // Do some time-limited stuff here
+        // (if this could block forever, then there's no guarantee the signal will have any
+        // effect).
+        print!(".");
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        std::io::stdout().flush()?;
+    }
+    println!("\nbye");
+    Ok(())
 }
