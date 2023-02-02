b67ff56fe19f98e1f9c28cb2174c70e7bc750aa8
Youva GAUDE <ygaude@student.42.fr>
2023-02-02 15:19:14 +0100

    working-ish basic unix socket example with signal handling note: UnixListener::accept() is blocking, which makes the server wait for one last connexion after receiving SIGHUP

--- taskmasterctl/src/main.rs
+++ taskmasterctl/src/main.rs
@@ -5,12 +5,31 @@ use std::os::unix::net::UnixStream;
 const SOCKET_PATH: &str = "/tmp/taskmaster.sock";
 
 fn main() -> Result<()> {
-    let mut stream = UnixStream::connect(SOCKET_PATH).context("Could not create stream")?;
-    stream
-        .write_all(b"hello")
+    let message = std::env::args().skip(1).collect::<Vec<_>>().join(" ");
+    if !message.is_empty() {
+        let mut unix_stream =
+            UnixStream::connect(SOCKET_PATH).context("Could not create stream")?;
+        write_request_and_shutdown(&mut unix_stream, message)?;
+        read_from_stream(&mut unix_stream)?;
+    }
+    Ok(())
+}
+
+fn write_request_and_shutdown(unix_stream: &mut UnixStream, message: String) -> Result<()> {
+    unix_stream
+        .write_all(message.as_bytes())
         .context("Failed at writing onto the unix stream")?;
+    unix_stream
+        .shutdown(std::net::Shutdown::Write)
+        .context("Could not shutdown writing on the stream")?;
+    Ok(())
+}
+
+fn read_from_stream(unix_stream: &mut UnixStream) -> Result<()> {
     let mut response = String::new();
-    // stream.read_to_string(&mut response)?;
-    println!("{response}");
+    unix_stream
+        .read_to_string(&mut response)
+        .context("Failed at reading from the unix stream")?;
+    println!("response: {}", response);
     Ok(())
 }
--- taskmasterd/src/main.rs
+++ taskmasterd/src/main.rs
@@ -13,22 +13,24 @@ fn main() -> Result<()> {
             .with_context(|| format!("could not delete previous socket at {:?}", SOCKET_PATH))?;
     }
     let unix_listener = UnixListener::bind(SOCKET_PATH)?;
-    let (mut stream, _) = unix_listener
-        .accept()
-        .context("Failed at accepting a connection on the unix listener")?;
     let term = Arc::new(AtomicBool::new(false));
     let mut response = String::new();
     signal_hook::flag::register(signal_hook::consts::SIGHUP, Arc::clone(&term))?;
-    println!("hi");
-    // while !term.load(Ordering::Relaxed) {
-    // std::thread::sleep(std::time::Duration::from_millis(100));
-    stream.read_to_string(&mut response)?;
-    eprintln!("response: {}", response);
-    // if response.contains("hello") {
-    stream.write_all(b"world")?;
-    // break;
-    // }
-    // }
-    println!("\nbye");
+    while !term.load(Ordering::Relaxed) {
+        let (mut stream, _) = unix_listener
+            .accept()
+            .context("Failed at accepting a connection on the unix listener")?;
+        stream
+            .read_to_string(&mut response)
+            .context("Failed at reading the request")?;
+        eprintln!("message: {}", response);
+        if response == "quit" {
+            break;
+        }
+        stream
+            .write_all(b"ack")
+            .context("Failed at writing the response")?;
+        response.clear();
+    }
     Ok(())
 }
