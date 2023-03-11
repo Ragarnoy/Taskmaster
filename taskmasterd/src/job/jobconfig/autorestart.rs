f3a4b18dcc182b046a26ff120bc828c90e1316ae
Tiago Lernould <tiagolernould@gmail.com>
2023-03-11 21:29:32 +0100

    switch to always and never for autorestart to prevent deser error

--- taskmasterd/src/job/jobconfig/autorestart.rs
+++ taskmasterd/src/job/jobconfig/autorestart.rs
@@ -3,8 +3,8 @@ use serde::Deserialize;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum AutoRestart {
-    True,
+    Always,
     #[default]
     Unexpected,
-    False,
+    Never,
 }
